f01235bcac1c31840e381c3ec647c8d012e28245
shivam gupta <shivamgupta150703@gmail.com>
2024-07-03 19:53:54 +0530

    reusable login and register form and client side routing

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import {Route,Routes} from 'react-router-dom'
+import Homepage from './pages/HomePage';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
