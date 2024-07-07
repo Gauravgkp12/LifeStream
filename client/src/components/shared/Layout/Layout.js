864edda30500d5fd99eab9a7710bf16eb5ffcd46
Vinay kumar <vin6287183337@gmail.com>
2024-07-07 18:50:20 +0530

    Design homepage layout and UI components

---
+++ client/src/components/shared/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <div className="header">
+        <Header />
+      </div>
+     
+       
+        <div className="col-md-9">{children}</div>
+      
+    </>
+  );
+};
+
+export default Layout;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { useSelector } from "react-redux";
 import Spinner from '../components/shared/Spinner';
 import { toast } from 'react-toastify';
+import Layout from '../components/shared/Layout/Layout';
 
 const HomePage = () => {
    const { loading, error, user } = useSelector((state) => state.auth);
   return (
-    <>
+    <Layout>
      {error && <span>{toast(error)}</span>}
       {loading?<Spinner/>:(
         <>
@@ -14,7 +15,7 @@ const HomePage = () => {
         </>
       )}
        
-    </>
+    </Layout>
   )
 }
 
