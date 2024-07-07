92d954ef9cd33033a242a015b8c7d4c64fb2b23b
Vinay kumar <vin6287183337@gmail.com>
2024-07-07 18:29:14 +0530

    Added functionality for loading state and Handling Error Messages

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,19 +1,32 @@
-import React from 'react'
-import Form from '../../components/shared/Form/Form'
+import React from "react";
+import Form from "../../components/shared/Form/Form";
+import { useSelector } from "react-redux";
+import Spinner from "./../../components/shared/Spinner";
+import { toast } from 'react-toastify';
 
 const Login = () => {
+  const { loading, error } = useSelector((state) => state.auth);
   return (
     <>
-      <div className='row g-0'>
-        <div className='col-md-8 form-banner'>
-            <img src='./assets/images/banner1.jpeg' alt='loginImage'/>
+       {error && <span>{toast(error)}</span>}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="row g-0">
+          <div className="col-md-8 form-banner">
+            <img src="./assets/images/banner1.jpeg" alt="loginImage" />
+          </div>
+          <div className="col-md-4 form-container">
+            <Form
+              formTitle={"Login Page"}
+              submitBtn={"Login"}
+              formType={"login"}
+            />
+          </div>
         </div>
-        <div className='col-md-4 form-container'>
-            <Form formType={'login'} formTitle={'Login Page'} submitBtn={'Login'}/>
-        </div>
-      </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
