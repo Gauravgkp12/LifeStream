3b2ea2e4a562dcee952e0466bfc1a94c17f98e50
Vinay kumar <vin6287183337@gmail.com>
2024-07-07 17:43:36 +0530

    Added ProtectedRoute and PublicRoute Functionality

--- client/src/redux/features/auth/authActions.js
+++ client/src/redux/features/auth/authActions.js
@@ -11,7 +11,7 @@ export const userLogin = createAsyncThunk(
       if (data.success) {
         toast.success(data.message);
         localStorage.setItem("token", data.token);
-        // window.location.replace("/");
+        window.location.replace("/");
       }
       return data;
     } catch (error) {
@@ -58,6 +58,27 @@ export const userRegister = createAsyncThunk(
         alert("User Registerd Successfully");
         window.location.replace("/login");
         // toast.success("User Registerd Successfully");
+      }
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
+
+
+//current user
+export const getCurrentUser = createAsyncThunk(
+  "auth/getCurrentUser",
+  async ({ rejectWithValue }) => {
+    try {
+      const res = await API.get("/auth/current-user");
+      if (res.data) {
+        return res?.data;
       }
     } catch (error) {
       console.log(error);
